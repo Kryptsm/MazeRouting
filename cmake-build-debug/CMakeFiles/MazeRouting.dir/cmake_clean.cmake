0b1b1022084b4a1992224ec7190d3fa5d99c7d30
Kryptsm <srwalsh@smu.edu>
2020-04-29 15:54:17 -0400

    Second committ

---
+++ cmake-build-debug/CMakeFiles/MazeRouting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MazeRouting.dir/main.cpp.o"
+  "MazeRouting"
+  "MazeRouting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MazeRouting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
