ef716fe9c536842063acaeb8ea384c2974d57d04
Kryptsm <srwalsh@smu.edu>
2020-04-29 15:59:31 -0400

    Added output functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
+
+using namespace  std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    ofstream output;
+
+    output.open("output.txt");
+    output << "Hello, World!" << endl;
+
     return 0;
 }
